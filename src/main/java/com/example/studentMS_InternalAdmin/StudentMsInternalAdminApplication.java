e137871176ee104b782bc3ed2fa5c0cb5dc401db
sophearith168-hengheng <himsophearith44@gmail.com>
2026-07-31 10:17:08 +0700

    first commit

---
+++ src/main/java/com/example/studentMS_InternalAdmin/StudentMsInternalAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentMS_InternalAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMsInternalAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMsInternalAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentMS_InternalAdmin/StudentMsInternalAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentMS_InternalAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMsInternalAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
